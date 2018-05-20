cb9bf936f4f65fd339bdda30cccd47ec2eeadadd
Alex Zhang <alexgzhang92@gmail.com>
2018-05-20 20:29:10 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -14,8 +14,7 @@ def won?(board)
     position_1 = board[windex_1]
     position_2 = board[windex_2]
     position_3 = board[windex_3]
-    if position_1 == "X" && position_2 = "X" && position_3 == "X"
-      return combo
+    position_1 == "X" && position_2 = "X" && position_3 == "X"
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return combo
     end
