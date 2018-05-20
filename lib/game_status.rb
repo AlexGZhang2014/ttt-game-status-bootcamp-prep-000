e1c1ecc09562e0eb571d488b9f7744c812b5dcf4
Alex Zhang <alexgzhang92@gmail.com>
2018-05-20 19:58:46 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,7 +24,7 @@ def won?(board)
 end
 
 def full?(board)
-  final_board = board.select {|element| element.position_taken?}
+  final_board = board.select {|element| element == "X" || element == "O"}
   if final_board.length == 9
     return true
   else
